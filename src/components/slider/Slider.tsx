9df07b3360382a15b990c3c4812d7d6c461dbd39
huyjaky <huyphanuyg@gmail.com>
2023-04-07 18:21:10 +0700

    chinh lai title

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -5,9 +5,12 @@ const Slider = () => {
     <div className="w-full h-[500px] bg-coffeeImg bg-cover mt-[30px] rounded-[30px] relative flex text-black">
       <div className="bg-slate-700 opacity-70 w-full h-full rounded-[30px] relative"></div>
       <div className="absolute w-full h-full flex text-slate-100">
-        <Link href="/" className="m-auto font-bold text-[50px]">
-          <h1>BUY ME NOW</h1>
-        </Link>
+        <div  className="m-auto  text-center">
+          <div className='mb-4'>BEST PLACE TO BUY COFFEE</div>
+          <Link className='font-bold text-[50px]' href="/">
+            <div>LET BUY ME</div>
+          </Link>
+        </div>
       </div>
     </div>
   );
